cef60c6661c08651180b421bf936e461ddd204c3
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-09-19 22:10:16 +0200

    start

--- Theme WP/glendaleliquor/footer.php
+++ Theme WP/glendaleliquor/footer.php
@@ -193,6 +193,192 @@
         </div>
     </div>
 </footer>
+
+<script>
+    function initMap() {
+        var uluru = {lat: 34.10409804762296, lng: -118.22702485730728};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 16,
+            center: uluru,
+            mapTypeControl: false,
+            scrollwheel: false,
+            zoomControl: false,
+            disableDefaultUI: true,
+            styles:   [
+                {
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#f5f5f5"
+                        }
+                    ]
+                },
+                {
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#616161"
+                        }
+                    ]
+                },
+                {
+                    "elementType": "labels.text.stroke",
+                    "stylers": [
+                        {
+                            "color": "#f5f5f5"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "administrative.land_parcel",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#bdbdbd"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#eeeeee"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#757575"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi.park",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#e5e5e5"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi.park",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#9e9e9e"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#ffffff"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#757575"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#dadada"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#616161"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#9e9e9e"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit.line",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#e5e5e5"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit.station",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#eeeeee"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#c9c9c9"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#9e9e9e"
+                        }
+                    ]
+                }
+            ]
+        });
+
+
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map,
+
+        });
+    }
+</script>
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDiyT05YehIdz2LrV-QOeRa5M18WfKtlnY&callback=initMap">
+</script>
+
   <?php wp_footer(); ?>
 	</body>
 </html>
--- Theme WP/glendaleliquor/inc/enqueue.php
+++ Theme WP/glendaleliquor/inc/enqueue.php
@@ -4,9 +4,15 @@ add_action( 'wp_enqueue_scripts', 'add_styles' );
 add_action( 'wp_enqueue_scripts', 'add_scripts' );
 
 function add_styles() {
+    wp_enqueue_style('normalize', get_template_directory_uri().'/css/normalize.css');
     wp_enqueue_style('fancybox', get_template_directory_uri().'/css/jquery.fancybox.min.css');
+    wp_enqueue_style('fonts', get_template_directory_uri().'/fonts/FA5PRO-master/css/all.css');
+    wp_enqueue_style('nice-select', get_template_directory_uri().'/css/nice-select.css');
     wp_enqueue_style('swiper', get_template_directory_uri().'/css/swiper.min.css');
-    wp_enqueue_style('styles', get_template_directory_uri().'/css/style.css', array(), rand(1111, 9999));
+    wp_enqueue_style('ion', get_template_directory_uri().'/css/ion.rangeSlider.min.css');
+    wp_enqueue_style('intlTelInput', get_template_directory_uri().'/css/intlTelInput.min.css');
+    wp_enqueue_style('air-datepicker', get_template_directory_uri().'/css/air-datepicker.css');
+    wp_enqueue_style('styles', get_template_directory_uri().'/css/styles.css', array(), rand(1111, 9999));
     wp_enqueue_style('responsive', get_template_directory_uri().'/css/responsive.css', array(), rand(1111, 9999));
     wp_enqueue_style( 'theme', get_stylesheet_uri() );
 
@@ -15,21 +21,24 @@ function add_styles() {
 
 function add_scripts() {
 
-    wp_enqueue_script("jquery-ui-core", array('jquery'));
-    wp_enqueue_script("jquery-ui-accordion", array('jquery','jquery-ui-core'));
     wp_enqueue_script( 'fancyboxjs', get_template_directory_uri() . '/js/jquery.fancybox.min.js', array('jquery'), false, true);
     wp_enqueue_script( 'dropzone', 'https://unpkg.com/dropzone@6.0.0-beta.1/dist/dropzone-min.js', array('jquery'), false, true);
     wp_enqueue_script('jqueryvalidation',  'https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.min.js', array(), false, 1);
     wp_enqueue_script( 'maskjs', get_template_directory_uri() . '/js/jquery.mask.js', array('jquery'), false, true);
     wp_enqueue_script( 'swiperjs', get_template_directory_uri() . '/js/swiper.js', array('jquery'), false, true);
+    wp_enqueue_script( 'select', get_template_directory_uri() . '/js/jquery.nice-select.min.js', array('jquery'), rand(1111, 9999), true);
+    wp_enqueue_script( 'sticky', get_template_directory_uri() . '/js/jquery.sticky.js', array('jquery'), rand(1111, 9999), true);
+    wp_enqueue_script( 'ion', get_template_directory_uri() . '/js/ion.rangeSlider.min.js', array('jquery'), rand(1111, 9999), true);
+    wp_enqueue_script( 'intlTelInput', get_template_directory_uri() . '/js/intlTelInput.min.js', array('jquery'), rand(1111, 9999), true);
+    wp_enqueue_script( 'air-datepicker', get_template_directory_uri() . '/js/air-datepicker.js', array('jquery'), rand(1111, 9999), true);
     wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array('jquery'), rand(1111, 9999), true);
+    wp_enqueue_script( 'action', get_template_directory_uri() . '/js/action.js', array('jquery'), rand(1111, 9999), true);
 
-    wp_localize_script('script', 'globals',
+
+    wp_localize_script('action', 'globals',
         array(
             'url' => admin_url('admin-ajax.php'),
             'template' => get_template_directory_uri(),
-            'upload'=>admin_url( 'admin-ajax.php?action=handle_dropped_media' ),
-            'delete'=>admin_url( 'admin-ajax.php?action=handle_deleted_media' ),
         )
     );
 
