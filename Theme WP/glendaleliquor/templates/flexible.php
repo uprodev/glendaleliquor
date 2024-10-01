6fb74606354eb8d0b52ac354becca5c1b527fdab
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-10-01 22:56:06 +0200

    cta

--- Theme WP/glendaleliquor/templates/flexible.php
+++ Theme WP/glendaleliquor/templates/flexible.php
@@ -12,42 +12,6 @@ if( have_rows('content', $id) ): ?>
 
 <?php endif; ?>
 
-<section class="item-2x">
-    <div class="content-width">
-        <div class="item item-1">
-            <div class="bg">
-                <img src="img/img-3-1.jpg" alt="">
-            </div>
-            <div class="wrap">
-                <div class="text">
-                    <h3>New collection</h3>
-                    <p>Check it out! Our new summer collection for 2024 is already in stock.</p>
-                    <div class="link-wrap">
-                        <a href="#" class="link"><i class="fa-light fa-location-arrow-up"></i>View all</a>
-                    </div>
-                </div>
-                <div class="label">
-                    <p>from $19.99</p>
-                </div>
-            </div>
-        </div>
-        <div class="item item-2">
-            <div class="bg">
-                <img src="img/img-3-2.jpg" alt="">
-            </div>
-            <div class="wrap">
-                <div class="text">
-                    <h3>try it out!</h3>
-                    <p>Your Destination for Quality Spirits and Fine Wines</p>
-                    <div class="link-wrap">
-                        <a href="#" class="link"><i class="fa-light fa-location-arrow-up"></i>View all</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
 <section class="products">
     <div class="content-width">
         <div class="title-wrap">
