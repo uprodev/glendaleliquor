ee0c16ff785809601c9e3deab029dabaa0cd0e2c
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-10-07 23:45:04 +0200

    checkout

--- Theme WP/glendaleliquor/woocommerce/checkout/form-checkout.php
+++ Theme WP/glendaleliquor/woocommerce/checkout/form-checkout.php
@@ -39,129 +39,23 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
                 <?php do_action( 'woocommerce_checkout_billing' ); ?>
 
-            </div>
-            <div class="aside">
-                <div class="title">
-                    <h6>Order summary</h6>
-                </div>
-                <div class="item-wrap">
-                    <p class="name">1 item</p>
-                    <div class="item">
-                        <figure>
-                            <a href="#">
-                                <img src="img/img-11.png" alt="">
-                            </a>
-                        </figure>
-                        <div class="text">
-                            <h6><a href="#">1 x 365 Gevorkian Strawberry Wine 750 ML</a></h6>
-                            <p class="mob">1 item</p>
-                        </div>
-                        <div class="price">
-                            <div class="col">
-                                <p class="mob">Total (USD)</p>
-                                <p>$15.99</p>
-                            </div>
-                            <p class="mob"><a href="#">Edit</a></p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="total-wrap">
-                    <div class="total-item">
-                        <p>Subtotal:</p>
-                        <p>$15.99</p>
-                    </div>
-                    <div class="total-item">
-                        <p>Shipping:</p>
-                        <p>Free</p>
-                    </div>
-                    <div class="total-item">
-                        <p>Default Tax Class</p>
-                        <p>$0.00</p>
-                    </div>
-                    <div class="total-item">
-                        <p>Promo/Gift Certificate</p>
-                        <p></p>
-                    </div>
-                </div>
-
-                <div class="total-line">
-                    <p>Total (USD)</p>
-                    <p>$15.99</p>
-                </div>
-
-                <div class="payment-wrap">
-                    <p>Payment</p>
-                    <div class="input-wrap input-wrap-radio">
-                        <div class="wrap-radio">
-                            <input type="radio" name="radio" id="radio-1" checked>
-                            <label for="radio-1">Credit card
-                                <img src="img/icon-9-1.svg" alt="">
-                                <img src="img/icon-9-2.svg" alt="">
-                                <img src="img/icon-9-3.svg" alt="">
-                            </label>
-                        </div>
-                        <div class="wrap-active">
-                            <div class="input-wrap">
-                                <label for="card-1">Credit Card Number</label>
-                                <input type="text" name="card-1" id="card-1" placeholder="">
-                            </div>
-                            <div class="input-wrap input-wrap-2">
-                                <label for="card-2">Expiration</label>
-                                <input type="text" name="card-2" id="card-2" placeholder="MM/YY">
-                            </div>
-                            <div class="input-wrap">
-                                <label for="card-3">Name on Card</label>
-                                <input type="text" name="card-3" id="card-3" placeholder="">
-                            </div>
-                            <div class="input-wrap input-wrap-2">
-                                <label for="card-4">CVV</label>
-                                <input type="text" name="card-4" id="card-4" placeholder="MM/YY">
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div class="input-wrap input-wrap-radio wrap-radio">
-                        <input type="radio" name="radio" id="radio-2">
-                        <label for="radio-2">Amazon Pay
-                            <img src="img/icon-9-4.svg" alt="">
-                        </label>
-                    </div>
-                </div>
-                <div class="input-wrap-submit">
-                    <button type="submit" class="btn-default btn-medium"><span>Place order</span></button>
-                </div>
-            </div>
-            <?php if ( $checkout->get_checkout_fields() ) : ?>
-
-                <?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
-
-                <div class="col2-set" id="customer_details">
-                    <div class="col-1">
-                        <?php do_action( 'woocommerce_checkout_billing' ); ?>
-                    </div>
-
-                    <div class="col-2">
-                        <?php do_action( 'woocommerce_checkout_shipping' ); ?>
-                    </div>
-                </div>
+                <?php do_action( 'woocommerce_checkout_shipping' ); ?>
 
                 <?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
-
-            <?php endif; ?>
-
-            <?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
-
-            <h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
+                
+            </div>
 
             <?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
-            <div id="order_review" class="woocommerce-checkout-review-order">
+
+            <div id="order_review" class="aside woocommerce-checkout-review-order">
+
                 <?php do_action( 'woocommerce_checkout_order_review' ); ?>
+
             </div>
 
             <?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+
 
         </form>
     </div>
--- Theme WP/glendaleliquor/woocommerce/checkout/review-order.php
+++ Theme WP/glendaleliquor/woocommerce/checkout/review-order.php
@@ -17,94 +17,98 @@
 
 defined( 'ABSPATH' ) || exit;
 ?>
-<table class="shop_table woocommerce-checkout-review-order-table">
-	<thead>
-		<tr>
-			<th class="product-name"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
-			<th class="product-total"><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php
-		do_action( 'woocommerce_review_order_before_cart_contents' );
 
-		foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
-			$_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+<div class="title">
+    <h6>Order summary</h6>
+</div>
 
-			if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
-				?>
-				<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
-					<td class="product-name">
-						<?php echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) ) . '&nbsp;'; ?>
-						<?php echo apply_filters( 'woocommerce_checkout_cart_item_quantity', ' <strong class="product-quantity">' . sprintf( '&times;&nbsp;%s', $cart_item['quantity'] ) . '</strong>', $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-						<?php echo wc_get_formatted_cart_item_data( $cart_item ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-					</td>
-					<td class="product-total">
-						<?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-					</td>
-				</tr>
-				<?php
-			}
-		}
+<div class="item-wrap">
+    <p class="name">
+        <?php $cart_count = WC()->cart->get_cart_contents_count();
 
-		do_action( 'woocommerce_review_order_after_cart_contents' );
-		?>
-	</tbody>
-	<tfoot>
+        if ($cart_count == 1) {
+            echo $cart_count . __(' item', 'glendaleliquor');
+        } else {
+            echo $cart_count . __(' items', 'glendaleliquor');
+        }?>
+    </p>
 
-		<tr class="cart-subtotal">
-			<th><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
-			<td><?php wc_cart_totals_subtotal_html(); ?></td>
-		</tr>
+    <?php
+    do_action( 'woocommerce_review_order_before_cart_contents' );
 
-		<?php foreach ( WC()->cart->get_coupons() as $code => $coupon ) : ?>
-			<tr class="cart-discount coupon-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
-				<th><?php wc_cart_totals_coupon_label( $coupon ); ?></th>
-				<td><?php wc_cart_totals_coupon_html( $coupon ); ?></td>
-			</tr>
-		<?php endforeach; ?>
+    foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+        $_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+        $_id = $_product->get_ID();
 
-		<?php if ( WC()->cart->needs_shipping() && WC()->cart->show_shipping() ) : ?>
+        if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
+            ?>
+            <div class="item">
+                <figure>
+                    <a href="<?= get_permalink($_id);?>">
+                        <img src="<?= get_the_post_thumbnail_url($_id);?>" alt="">
+                    </a>
+                </figure>
+                <div class="text">
+                    <h6><a href="<?= get_permalink($_id);?>"><?= apply_filters( 'woocommerce_checkout_cart_item_quantity', sprintf( '%s&nbsp;x&nbsp;', $cart_item['quantity'] ), $cart_item, $cart_item_key ) . wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) ); ?></a></h6>
+                    <p class="mob"><?= $cart_item['quantity'];?> item</p>
+                </div>
+                <div class="price">
+                    <div class="col">
+                        <p class="mob">Total (USD)</p>
+                        <p><?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+                    </div>
+                    <p class="mob"><a href="#">Edit</a></p>
+                </div>
+            </div>
+            <?php
+        }
+    }
 
-			<?php do_action( 'woocommerce_review_order_before_shipping' ); ?>
+    do_action( 'woocommerce_review_order_after_cart_contents' );
+    ?>
 
-			<?php wc_cart_totals_shipping_html(); ?>
+</div>
 
-			<?php do_action( 'woocommerce_review_order_after_shipping' ); ?>
+<div class="total-wrap">
+    <div class="total-item">
+        <p><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?>:</p>
+        <p><?php wc_cart_totals_subtotal_html(); ?></p>
+    </div>
+    <div class="total-item">
+        <p>Shipping:</p>
+        <p>Free</p>
+    </div>
+    <?php  if ( wc_tax_enabled() && ! WC()->cart->display_prices_including_tax() ) : ?>
+        <?php if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) : ?>
+            <?php foreach ( WC()->cart->get_tax_totals() as $code => $tax ) : // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited ?>
+                <div class="total-item">
+                    <p>Default Tax Class</p>
+                    <p><?php echo wp_kses_post( $tax->formatted_amount ); ?></p>
+                </div>
+            <?php endforeach; ?>
+        <?php else : ?>
+            <div class="total-item">
+                <p><?php echo esc_html( WC()->countries->tax_or_vat() ); ?></p>
+                <p><?php wc_cart_totals_taxes_total_html(); ?></p>
+            </div>
+        <?php endif; ?>
+    <?php endif; ?>
 
-		<?php endif; ?>
+    <?php foreach ( WC()->cart->get_coupons() as $code => $coupon ) : ?>
+        <div class="total-item">
+            <p>Promo/Gift Certificate</p>
+            <p><?php wc_cart_totals_coupon_html( $coupon ); ?></p>
+        </div>
+    <?php endforeach; ?>
 
-		<?php foreach ( WC()->cart->get_fees() as $fee ) : ?>
-			<tr class="fee">
-				<th><?php echo esc_html( $fee->name ); ?></th>
-				<td><?php wc_cart_totals_fee_html( $fee ); ?></td>
-			</tr>
-		<?php endforeach; ?>
+</div>
 
-		<?php if ( wc_tax_enabled() && ! WC()->cart->display_prices_including_tax() ) : ?>
-			<?php if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) : ?>
-				<?php foreach ( WC()->cart->get_tax_totals() as $code => $tax ) : // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited ?>
-					<tr class="tax-rate tax-rate-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
-						<th><?php echo esc_html( $tax->label ); ?></th>
-						<td><?php echo wp_kses_post( $tax->formatted_amount ); ?></td>
-					</tr>
-				<?php endforeach; ?>
-			<?php else : ?>
-				<tr class="tax-total">
-					<th><?php echo esc_html( WC()->countries->tax_or_vat() ); ?></th>
-					<td><?php wc_cart_totals_taxes_total_html(); ?></td>
-				</tr>
-			<?php endif; ?>
-		<?php endif; ?>
+<?php do_action( 'woocommerce_review_order_before_order_total' ); ?>
 
-		<?php do_action( 'woocommerce_review_order_before_order_total' ); ?>
+<div class="total-line">
+    <p><?php esc_html_e( 'Total (USD)', 'woocommerce' ); ?></p>
+    <p><?php wc_cart_totals_order_total_html(); ?></p>
+</div>
 
-		<tr class="order-total">
-			<th><?php esc_html_e( 'Total', 'woocommerce' ); ?></th>
-			<td><?php wc_cart_totals_order_total_html(); ?></td>
-		</tr>
+<?php do_action( 'woocommerce_review_order_after_order_total' ); ?>
 
-		<?php do_action( 'woocommerce_review_order_after_order_total' ); ?>
-
-	</tfoot>
-</table>
