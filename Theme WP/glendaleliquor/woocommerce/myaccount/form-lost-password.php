8b2849a6b9990a91ae8733e8c0c4539d6ad621dd
Stanislav-pr <77438513+Stanislav-pr@users.noreply.github.com>
2024-10-28 16:31:59 +0100

    Update form-lost-password.php

--- Theme WP/glendaleliquor/woocommerce/myaccount/form-lost-password.php
+++ Theme WP/glendaleliquor/woocommerce/myaccount/form-lost-password.php
@@ -21,11 +21,11 @@ do_action( 'woocommerce_before_lost_password_form' );
 ?>
 <section class="login">
     <div class="content-width">
-        <form method="post" class="woocommerce-ResetPassword lost_reset_password">
+        <form method="post" class="default-form woocommerce-ResetPassword lost_reset_password">
 
             <p><?php echo apply_filters( 'woocommerce_lost_password_message', esc_html__( 'Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'woocommerce' ) ); ?></p><?php // @codingStandardsIgnoreLine ?>
 
-            <p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
+            <p class="input-wrap input-wrap-full">
                 <label for="user_login"><?php esc_html_e( 'Username or email', 'woocommerce' ); ?>&nbsp;<span class="required" aria-hidden="true">*</span><span class="screen-reader-text"><?php esc_html_e( 'Required', 'woocommerce' ); ?></span></label>
                 <input class="woocommerce-Input woocommerce-Input--text input-text" type="text" name="user_login" id="user_login" autocomplete="username" required aria-required="true" />
             </p>
